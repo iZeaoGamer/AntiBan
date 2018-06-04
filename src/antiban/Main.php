60460069f6e258a0bb3d7afbb36cbdb204809bff
iZeaoGamer <jameshayward14@icloud.com>
2018-06-04 07:55:30 +0100

    Update Main.php

--- src/antiban/Main.php
+++ src/antiban/Main.php
@@ -1,4 +1,5 @@
 <?php
+
   namespace antiban;
 
   use pocketmine\plugin\PluginBase;
@@ -13,14 +14,12 @@
   
   class Main extends PluginBase implements Listener
   {
-
     public function onEnable()
     {
       if(!file_exists($this->getDataFolder() . "config.yml")){
               @mkdir($this->getDataFolder());
               file_put_contents($this->getDataFolder()."config.yml", $this->getResource("config.yml"));
             }
-
       $this->getLogger()->info(TF::GREEN . "Enabled.");
       $this->getServer()->getPluginManager()->registerEvents($this , $this);
     }
@@ -32,142 +31,84 @@
             $op = "$getop[0]";
             $sName = $sender->getName();
             if($sName == $op){
-
       switch($cmd->getName())
       {
-
         case "antiban":
-
           if(!(isset($args[0])))
           {
-
             $sender->sendMessage(TF::RED . "§bPlease use: §a/antiban <add | list> [name]");
-
             return true;
-
           }
           else if($args[0] === "add")
           {
-
             if(isset($args[1]))
             {
-
               $name = $args[1];
-
               $players = $this->cfg->get("players");
-
               if(isset($players[array_search($players, $name)]))
               {
-
                 $sender->sendMessage(TF::RED . "§2Player§c " . $name . " §2is already in the antiban list.");
-
                 return true;
   
               }
               else
               {
-
                 array_push($players, $name);
-
                 $this->cfg->set("players", $players);
-
                 $this->cfg->save();
-
                 $sender->sendMessage(TF::GREEN . "§dSuccessfully added player§5 " . $name . "§d to the antiban list.");
-
                 return true;
-
               }
-
             }
             else
             {
-
               $sender->sendMessage(TF::RED . "§bPlease use: §a/antiban add <name>");
-
               return true;
-
             }
-
           }
           else if($args[0] === "list")
           {
-
             $players = $this->cfg->get("players");
-
             $list = "";
-
             foreach($players as $player)
             {
-
               $list .= $player . ", ";
-
             }
-
             $sender->sendMessage(TF::YELLOW . "§6-- AntiBan Player List --");
-
             $sender->sendMessage(TF::PURPLE . $list);
-
             return true;
-
           }
           else
           {
-
             $sender->sendMessage(TF::RED . "§bPlease use: §a /antiban <add | list> [name]");
-
             return true;
-
           }
-
         break;
-
       }
-
     }
-
+}
     public function onCommandPreprocess(PlayerCommandPreprocessEvent $event)
     {
       $command = explode(" ", $event->getMessage());
-
       if($command[0] === "/ban" or $command[0] === "/ban-ip" or $command[0] === "/devban" or $command[0] === "/tban")
       {
-
         if(isset($command[1]))
         {
-
           $players = $this->cfg->get("players");
-
           foreach($players as $player)
           {
-
             if($command[1] === $player)
             {
-
               $sender->sendMessage(TF::RED . "§2The player you are trying to ban is in the AntiBan player list.");
-
               $event->setCancelled(true);
-
               return true;
-
             }
-
           }
-
         }
-
       }
-
     }
-
     public function onDisable()
     {
-
       $this->getLogger()->info(TF::RED . "Disabled.");
-
     }
-
   }
-  return true;
-  }
-}
